0d3719ed819c8d2e493ec105c873ac43ccd5a5b0
Andrey Violante <dinatosviolante@gmail.com>
2026-09-16 21:24:58 -0300

    feat: criar views de lista e novo com filtros

--- acervo/views.py
+++ acervo/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Livro
+from .forms import LivroForm
 
-# Create your views here.
+def lista_livros(request):
+    livros = Livro.objects.all()
+    
+    nome = request.GET.get('nome')
+    tipo = request.GET.get('tipo')
+    categoria = request.GET.get('categoria')
+    
+    if nome:
+        livros = livros.filter(titulo__icontains=nome)
+    if tipo:
+        livros = livros.filter(tipo_acervo=tipo)
+    if categoria:
+        livros = livros.filter(categoria=categoria)
+    
+    return render(request, 'acervo/lista.html', {'livros': livros})
+
+def novo_livro(request):
+    if request.method == 'POST':
+        form = LivroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista')
+    else:
+        form = LivroForm()
+    
+    return render(request, 'acervo/form.html', {'form': form})
